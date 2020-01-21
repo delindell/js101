82ca104052910db88b1d1ae7abd5ae0fb2b2af0b
delindell <daviselindell@gmail.com>
2020-01-20 21:05:23 -0600

    arrays beginning

---
+++ arrays.js
@@ -0,0 +1,16 @@
+console.log('arrays')
+
+//const junkDrawer = ['hi', 2, true, {name: DAvis Lindell}, [1, 2, 3]];
+
+
+//console.log(junkDrawer[2]);
+//junkDrawer.push('fluffy');
+
+const valuePrinter = (arr, idx) => {
+    return arr[idx];
+
+};
+
+console.log(valuePrinter([1, 2, 3, 4, 5,], 2))
+console.log(valuePrinter([1, 2, 3, 4, 5,], 2))
+
